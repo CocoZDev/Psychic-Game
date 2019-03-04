aed0f8f1814ed7618438d4cc5706376e8ba78dec
Guiyu Zhao <msguiyuzhao@gmail.com>
2019-03-03 21:07:36 -0800

    added show answer button and replaced alert with message in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,17 +7,22 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g",
 "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // This line of JavaScript "grabs" the main div on the page;
-var answersSoFar = document.getElementById("answersSoFar");
+var guessesSoFar = document.getElementById("guessesSoFar");
 var guesses = document.getElementById("guessesLeftCount");
 var wins = document.getElementById("winsCount");
 var losses = document.getElementById("lossesCount");
 
 // Creating variables to hold the number of wins, losses, and guesses left.
 //Staring count numbers
-var guesses = 10;
+var guesses = 5;
 var wins = 0;
 var losses = 0;  
 var hasWon = false;  
+
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+console.log(computerGuess);
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
@@ -25,8 +30,7 @@ document.onkeyup = function(event) {
 	// Determines which key was pressed.
 	var userGuess = event.key;
 
-	// Randomly chooses a choice from the options array. This is the Computer's guess.
-	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	gameHistoryTracking();
 
 	// This logic determines the outcome of the game (win/loss), and increments the appropriate number
 	// Compare user's guesses with the right answer
@@ -35,13 +39,12 @@ document.onkeyup = function(event) {
 		wins++;
 		guesses = 0;
 		hasWon = true;
-		alert ("Congratulations. You have gussed the right letter! Click Okay Button to play again.");
-
-	}
-
+		document.querySelector("#message").innerHTML = "You won!!! <br>You have gussed the right letter, <em><u>" + userGuess +"</u></em>. <br>To play again, just refresh this page.";
+	} 
 	// Otherwise, number of guesses decrease by 1 as user type
 	else {
-		guesses--;}
+		guesses--;
+	}
 	
 	// //If Guess reaches 0, add 1 to losses
 	if (guesses === 0) {
@@ -58,14 +61,19 @@ document.onkeyup = function(event) {
 	// Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
 	// Set the inner HTML contents of the #game div to our html string  
 
-	document.querySelector("#answersSoFar").innerHTML = document.querySelector("#answersSoFar").innerHTML + userGuess + ", ";
-	document.querySelector("#guessesLeftCount").innerHTML = guesses;
-	document.querySelector("#winsCount").innerHTML = wins;
-	document.querySelector("#lossesCount").innerHTML = losses;
-	
+	function gameHistoryTracking(){
+		document.querySelector("#guessesSoFar").innerHTML = document.querySelector("#guessesSoFar").innerHTML + userGuess + ", ";
+		document.querySelector("#guessesLeftCount").innerHTML = guesses;
+		document.querySelector("#winsCount").innerHTML = wins;
+		document.querySelector("#lossesCount").innerHTML = losses;
+	};	
 };
 
 
-// Bonus Event: Reveal the correct answer when user click on a button
+// Reveal the correct answer when user click on a button
+document.querySelector("button").click(function(){
+	document.querySelector("#correctAnswer").innerHTML = "Correct Answer: " + computerGuess;
+	console.log("Correct Answer: " + computerGuess);
+})
 
 // Bonus Event: Add a countdown timer
